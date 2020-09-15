5ff647608a26f3d5b8a098fcb255574eec405eb5
yixi <yixi991010@163.com>
2020-09-15 11:34:38 +0800

    平台端子模块

---
+++ mybatis/src/main/java/com/example/mybatis/mapper/ManagersDao.java
@@ -0,0 +1,16 @@
+package com.example.mybatis.mapper;
+
+import com.example.mybatis.entity.Managers;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 管理员表 Mapper 接口
+ * </p>
+ *
+ * @author hzp
+ * @since 2020-09-15
+ */
+public interface ManagersDao extends BaseMapper<Managers> {
+
+}
