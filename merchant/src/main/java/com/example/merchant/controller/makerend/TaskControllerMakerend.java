43b3874e5d9c4d8270c9f83d25e544cd8cea1fa3
jwei0401 <xiongjiawei>
2020-09-26 09:02:22 +0800

    0926

---
+++ merchant/src/main/java/com/example/merchant/controller/makerend/TaskControllerMakerend.java
@@ -0,0 +1,10 @@
+package com.example.merchant.controller.makerend;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(value = "小程序任务", tags = {"小程序任务"})
+@RestController(value = "makerend/task")
+public class TaskControllerMakerend {
+    
+}
