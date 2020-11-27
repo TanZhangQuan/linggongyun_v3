76e333e9bd77806570a46e8fdc77efccd8e5fa7f
tanzhangquan <1418541909@qq.com>
2020-11-27 17:10:30 +0800

    调整修改

--- common/src/main/java/com/example/common/util/IdCardUtils.java
+++ common/src/main/java/com/example/common/util/IdCardUtils.java
@@ -27,35 +27,4 @@ public class IdCardUtils {
         idCardInfo.put("address",words_result.get("住址").get("words").toString());
         return idCardInfo;
     }
-
-    public static Map<String,String> getIdCardInfo(String filePath,String id_card_side){
-        String accessToken = AuthService.getAuth();
-        String front = Idcard.idcard(filePath, accessToken, id_card_side);
-        Map<String,Map<String,Map<String,Object>>> mapMap = new HashMap<>();
-        Map<String,Map<String,Map<String,Object>>> map = JsonUtils.jsonToPojo(front, mapMap.getClass());
-        Map<String, Map<String, Object>> words_result = map.get("words_result");
-        Map<String,String> idCardInfo = new HashMap<>();
-        idCardInfo.put("name",words_result.get("姓名").get("words").toString());
-        idCardInfo.put("idCard",words_result.get("公民身份号码").get("words").toString());
-        idCardInfo.put("nation",words_result.get("民族").get("words").toString());
-        idCardInfo.put("sex",words_result.get("性别").get("words").toString());
-        idCardInfo.put("birth",words_result.get("出生").get("words").toString());
-        idCardInfo.put("address",words_result.get("住址").get("words").toString());
-        return idCardInfo;
-    }
-
-    public static Map<String,String> getIdCardInfo(String filePath,String id_card_side,String accessToken){
-        String front = Idcard.idcard(filePath, accessToken, id_card_side);
-        Map<String,Map<String,Map<String,Object>>> mapMap = new HashMap<>();
-        Map<String,Map<String,Map<String,Object>>> map = JsonUtils.jsonToPojo(front, mapMap.getClass());
-        Map<String, Map<String, Object>> words_result = map.get("words_result");
-        Map<String,String> idCardInfo = new HashMap<>();
-        idCardInfo.put("name",words_result.get("姓名").get("words").toString());
-        idCardInfo.put("idCard",words_result.get("公民身份号码").get("words").toString());
-        idCardInfo.put("nation",words_result.get("民族").get("words").toString());
-        idCardInfo.put("sex",words_result.get("性别").get("words").toString());
-        idCardInfo.put("birth",words_result.get("出生").get("words").toString());
-        idCardInfo.put("address",words_result.get("住址").get("words").toString());
-        return idCardInfo;
-    }
 }
