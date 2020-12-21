0df10bdc9c49cfed5f124d9b882a0ae1853bf7fd
tanzhangquan <1418541909@qq.com>
2020-12-21 20:25:12 +0800

    调整修改

--- merchant/src/main/java/com/example/merchant/vo/ExpressInfoVO.java
+++ merchant/src/main/java/com/example/merchant/vo/ExpressInfoVO.java
@@ -3,7 +3,6 @@ package com.example.merchant.vo;
 
 import com.example.common.util.ExpressLogisticsInfo;
 import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -15,12 +14,18 @@ import java.util.List;
 public class ExpressInfoVO implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    @ApiModelProperty("快递公司名称")
+    /**
+     * 快递公司名称
+     */
     private String expressCompanyName;
 
-    @ApiModelProperty("快递单号")
+    /**
+     * 快递单号
+     */
     private String expressCode;
 
-    @ApiModelProperty("快递物流信息")
+    /**
+     * 快递物流信息
+     */
     private List<ExpressLogisticsInfo> expressLogisticsInfos;
 }
