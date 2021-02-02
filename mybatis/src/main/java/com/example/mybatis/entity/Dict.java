d816419a47f189afdf762c5bd3e61e7d4fb02521
tanzhangquan <1418541909@qq.com>
2021-02-02 17:15:39 +0800

    调整修改

--- mybatis/src/main/java/com/example/mybatis/entity/Dict.java
+++ mybatis/src/main/java/com/example/mybatis/entity/Dict.java
@@ -1,14 +1,6 @@
 package com.example.mybatis.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
-import com.baomidou.mybatisplus.annotation.IdType;
-
-import java.time.LocalDateTime;
-
-import com.baomidou.mybatisplus.annotation.TableId;
-
-import java.io.Serializable;
-
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -54,11 +46,5 @@ public class Dict extends BaseEntity {
      * 字典备注
      */
     private String remark;
-
-    /**
-     * 是否已删除
-     */
-    private Boolean boolDeleted = true;
-
 
 }
